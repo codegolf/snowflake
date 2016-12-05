b8c7dfa901e8e772d8db56060f01aee0b61e5bd9
Martin Kleppe <kleppe@ubilabs.net>
2016-12-05 23:54:25 +0100

    add source for tweet

---
+++ tweet.js
@@ -0,0 +1,5 @@
+// tweet:
+eval(unescape(escape`𩁯𨱵𫑥𫡴𛡷𬡩𭁥𨀼𨱡𫡶𨑳𘁩𩀽𨠾𨀻𭱩𭁨𚁍𨑴𪀩𩡯𬠨𢐽𦐽𜀻𦐫𚰼𝰰𞱉👲𨑮𩁯𫐨𚐭𜐮𞐦𢐫𜐩𩡯𬠨𪐽𜐲𞱩𛐭𞱃👘🐾𨠮𩱥𭁃𫱮𭁥𮁴𨀲𩁠𛡦𪑬𫁒𩑣𭀨𝰰𚱘𛁳𚡪𚱣𚡙𚰷𜀬𜐬𜐩𯁼𦀬𠰨𛑃𚀨𨰽𨱯𬰨𨐽𤁉𛰳𚡩𚐩𚡪𛐨𬰽𬱩𫠨𨐩𚐪𦐩𚐩𪠽𪐾𝐦𙡉`.replace(/u../g,'')))
+
+// source:
+document.write`<canvas id=b>`;with(Math)for(I=Y=0;Y++<70;I=random()-1.9&I+1)for(i=12;i--;C=X=>b.getContext`2d`.fillRect(70+X,s*j+c*Y+70,1,1)||X,C(-C((c=cos(a=PI/3*i))*j-(s=sin(a))*Y)))j=i>5&&I
